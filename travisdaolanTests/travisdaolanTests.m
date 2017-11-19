98651fd7b82d00a4dfbde68d7b2748dbad448b3b
chen <1070438954@qq.com>
2017-11-19 20:30:09 +0800

    test

--- travisdaolanTests/travisdaolanTests.m
+++ travisdaolanTests/travisdaolanTests.m
@@ -16,7 +16,7 @@
 
 - (void)setUp {
     [super setUp];
-    NSAssert(true, @"挂了");
+    NSAssert(false, @"挂了");
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
